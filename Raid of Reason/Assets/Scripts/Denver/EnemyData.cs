3c6328d9b3f08d0286d30ec49b71080fbe398e74
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2019-07-19 17:16:23 +1000

    Added EnemyData class and EnemyManager class

---
+++ Raid of Reason/Assets/Scripts/Denver/EnemyData.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[System.Serializable]
+public enum EnemyType {
+    SUICIDE,
+    MELEE,
+    RANGE,
+    SPAWNER
+}
+
+[System.Serializable]
+public struct EnemyAttackRange {
+    public float min;
+    public float max;
+}
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyData : MonoBehaviour {
+
+    [SerializeField] private EnemyType m_type;
+    public EnemyType Type { get => m_type; }
+
+    public float ViewRange { get; private set; }
+    public int MaxHealth { get; private set; }
+    public int Health { get; private set; }
+    public EnemyAttackRange AttackRange { get; private set; }
+    public float AttackCooldown { get; private set; }
+    public float AttackTimer { get; private set; }
+    public bool Attacking { get; private set; }
+
+    public NavMeshAgent m_navMeshAgent { get; private set; }
+
+    // Afridi added this for the skill tree
+    public bool isDeadbByKenron = false;
+
+    /// <sumarry>
+    /// Initialises the enemy's values
+    /// </sumarry>
+    /// <param name="viewRange">
+    /// How far away enemy can see player
+    /// </param>
+    /// <param name="maxHealth">
+    /// Maximum health of enemy
+    /// </param>
+    /// <param name="attackRange">
+    /// Distance range in which enemy will attack
+    /// </param>
+    /// <param name="attackCooldown">
+    /// How often enemy will attack
+    /// </param>
+    public void Init(float viewRange, int maxHealth, EnemyAttackRange attackRange, float attackCooldown)
+    {
+        ViewRange = viewRange;
+
+        MaxHealth = maxHealth;
+        Health = MaxHealth;
+
+        AttackRange = attackRange;
+
+        AttackCooldown = attackCooldown;
+        AttackTimer = 0.0f;
+
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+}
---
+++ Raid of Reason/Assets/Scripts/Denver/EnemyManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("Enemy Type View Ranges")]
+    [SerializeField] private float m_suicideEnemyViewRange;
+    [SerializeField] private float m_meleeEnemyViewRange;
+    [SerializeField] private float m_rangeEnemyViewRange;
+    [SerializeField] private float m_spawnerEnemyViewRange;
+
+    [Header("Enemy Type Attack Ranges")]
+    [SerializeField] private float m_suicideEnemyAttackRange;
+    [SerializeField] private float m_meleeEnemyAttackRange;
+    [SerializeField] private EnemyAttackRange m_rangeEnemyAttackRange;
+    [SerializeField] private EnemyAttackRange m_spawnerEnemyAttackRange;
+
+    [Header("Enemy Type Attack Cooldowns")]
+    [SerializeField] private float m_suicideEnemyAttackCooldown;
+    [SerializeField] private float m_meleeEnemyAttackCooldown;
+    [SerializeField] private float m_rangeEnemyAttackCooldown;
+    [SerializeField] private float m_spawnerEnemyAttackCooldown;
+
+    [Header("Enemy Type Max Healths")]
+    [SerializeField] private int m_suicideEnemyMaxHealth;
+    [SerializeField] private int m_meleeEnemyMaxHealth;
+    [SerializeField] private int m_rangeEnemyMaxHealth;
+    [SerializeField] private int m_spawnerEnemyMaxHealth;
+
+    [Header("Enemy Type Damages")]
+    [SerializeField] private int m_suicideEnemyDamage;
+    [SerializeField] private int m_meleeEnemyDamage;
+    [SerializeField] private int m_rangeEnemyDamage;
+
+    [Header("Enemy Objects")]
+    [SerializeField] private EnemyData[] m_enemies;
+
+    // Start is called before the first frame update
+    void Start() {
+        InitEnemies();
+        InitBehaviourTrees();
+    }
+
+    private void InitEnemies() {
+        EnemyAttackRange suicideAttackRange;
+        suicideAttackRange.min = 0.0f;
+        suicideAttackRange.max = m_suicideEnemyAttackRange;
+
+        EnemyAttackRange meleeAttackRange;
+        meleeAttackRange.min = 0.0f;
+        meleeAttackRange.max = m_meleeEnemyAttackRange;
+
+        foreach (EnemyData enemy in m_enemies) {
+            switch (enemy.Type) {
+                case EnemyType.SUICIDE:
+                    enemy.Init(m_suicideEnemyViewRange, m_suicideEnemyMaxHealth, suicideAttackRange, m_suicideEnemyAttackCooldown);
+                    break;
+                
+                case EnemyType.MELEE:
+                    enemy.Init(m_meleeEnemyViewRange, m_meleeEnemyMaxHealth, meleeAttackRange, m_meleeEnemyAttackCooldown);
+                    break;
+
+                case EnemyType.RANGE:
+                    enemy.Init(m_rangeEnemyViewRange, m_rangeEnemyMaxHealth, m_rangeEnemyAttackRange, m_rangeEnemyAttackCooldown);
+                    break;
+
+                case EnemyType.SPAWNER:
+                    enemy.Init(m_spawnerEnemyViewRange, m_spawnerEnemyMaxHealth, m_spawnerEnemyAttackRange, m_spawnerEnemyAttackCooldown);
+                    break;
+
+                default:
+                    Debug.LogError("Failed to initiailise enemy!", this);
+                    break;
+            }
+        }
+    }
+
+    private void InitBehaviourTrees() {
+
+    }
+}
