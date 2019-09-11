de92894cc98dffa233a1fddde2d315f412b620c3
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2019-09-12 09:18:43 +1000

    Fixed bug where Nashorn wouldn't hit enemies when attack is pressed instead of held - Minor alterations to Suicide Enemy Prefab

--- Raid of Reason/Assets/Scripts/Afridi/SmashDamage.cs
+++ Raid of Reason/Assets/Scripts/Afridi/SmashDamage.cs
@@ -17,35 +17,32 @@ public class SmashDamage : MonoBehaviour
     {
         if (other.gameObject.tag == "Enemy")
         {
-            EnemyData enemy = other.gameObject.GetComponent<EnemyData>();
+			EnemyData enemy = other.gameObject.GetComponent<EnemyData>();
+		
+			GameManager.Instance.Nashorn.currentShield += GameManager.Instance.Nashorn.shieldGain;
 
-            if (enemy && XCI.GetAxis(XboxAxis.RightTrigger, GameManager.Instance.Nashorn.controller) > 0.1)
-            {
-                GameManager.Instance.Nashorn.currentShield += GameManager.Instance.Nashorn.shieldGain;
+			// Knock back enemies with every punch Nashorn lands.
+			Rigidbody rb = other.GetComponent<Rigidbody>();
+			if (rb != null)
+			{
+			    Vector3 direction = other.transform.position - GameManager.Instance.Nashorn.transform.position;
+			    direction.y = 0;
 
-                // Knock back enemies with every punch Nashorn lands.
-                Rigidbody rb = other.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    Vector3 direction = other.transform.position - GameManager.Instance.Nashorn.transform.position;
-                    direction.y = 0;
-
-                    rb.AddForce(direction.normalized * m_knockBackForce, ForceMode.Impulse);
-                }
-                enemy.TakeDamage(GameManager.Instance.Nashorn.GetDamage(), GameManager.Instance.Nashorn);
-				enemy.Stun(m_stunTime);
-                if (GameManager.Instance.Nashorn.m_skillUpgrades.Find(skill => skill.Name == "Shockwave"))
-                {
-
-                }
-                if (GameManager.Instance.Kenron != null)
-                {
-                    if (GameManager.Instance.Kenron.m_skillUpgrades.Find(skill => skill.Name == "Shuras Reckoning") && GameManager.Instance.Kenron.isActive == true)
-                    {
-                        other.GetComponent<StatusEffectManager>().ApplyBurn(4);
-                    }
-                }
-            }
+			    rb.AddForce(direction.normalized * m_knockBackForce, ForceMode.Impulse);
+			}
+			enemy.TakeDamage(GameManager.Instance.Nashorn.GetDamage(), GameManager.Instance.Nashorn);
+			enemy.Stun(m_stunTime);
+			if (GameManager.Instance.Nashorn.m_skillUpgrades.Find(skill => skill.Name == "Shockwave"))
+			{
+				// TODO: Shockwave stuff
+			}
+			if (GameManager.Instance.Kenron != null)
+			{
+			    if (GameManager.Instance.Kenron.m_skillUpgrades.Find(skill => skill.Name == "Shuras Reckoning") && GameManager.Instance.Kenron.isActive == true)
+			    {
+			        other.GetComponent<StatusEffectManager>().ApplyBurn(4);
+			    }
+			}
         }
     }
 }
