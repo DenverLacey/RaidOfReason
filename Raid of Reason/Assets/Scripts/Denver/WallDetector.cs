0dc283e34941b0c468a3b1ce5eddfca1c9fa67be
XavierSchneider <39175089+XavierSchneider@users.noreply.github.com>
2019-09-25 14:21:11 +1000

    Fixed the the projectile

--- Raid of Reason/Assets/Scripts/Denver/WallDetector.cs
+++ Raid of Reason/Assets/Scripts/Denver/WallDetector.cs
@@ -7,16 +7,11 @@ public class WallDetector : MonoBehaviour
 	[SerializeField]
 	private ProjectileMove m_projectile;
 
-	private bool CollidedWithWall(Collider other)
-	{
-		return !Utility.TagIsPlayerTag(other.tag) && other.tag != "Enemy" && other.tag != "EnemyManager" && other.name != m_projectile.name && other.name != name && other.name != "EnemyProjectile(Clone)";
-	}
-
 	private void OnTriggerEnter(Collider other)
 	{
 		bool active = m_projectile.gameObject.activeSelf;
 
-		if (CollidedWithWall(other))
+		if (other.gameObject.layer == LayerMask.NameToLayer("Environment"))
 		{
 			m_projectile.Destroy();
 		}
