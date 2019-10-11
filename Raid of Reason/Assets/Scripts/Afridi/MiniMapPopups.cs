0e9464c5e75085de2566fd2ad418e2060714bf30
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2019-10-11 17:00:30 +1100

    - Main Beta Scene Added

---
+++ Raid of Reason/Assets/Scripts/Afridi/MiniMapPopups.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMapPopups : MonoBehaviour
+{
+    public Camera miniMap;
+    public GameObject Objective;
+    public Image Arrow;
+    Vector3 centreView;
+
+    void Awake()
+    {
+        centreView = new Vector3(0.5f, 0.5f, 0.0f);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 objPosView = miniMap.WorldToViewportPoint(Objective.transform.localPosition);
+        if (objPosView.z < 0)
+        {
+            objPosView *= -1;
+        }
+
+        Vector3 dirView = (objPosView - centreView);
+        float distView = dirView.magnitude;
+        dirView.Normalize();
+
+        if (distView > 0.5f)
+        {
+            objPosView = centreView + dirView * 0.5f;
+        }
+
+        objPosView = miniMap.ViewportToScreenPoint(objPosView);
+        Arrow.transform.position = objPosView;
+    }
+}
--- Raid of Reason/Assets/Scripts/Afridi/ObjectiveMarker.cs
+++ Raid of Reason/Assets/Scripts/Afridi/ObjectiveMarker.cs
@@ -6,18 +6,26 @@ using UnityEngine;
 public class ObjectiveMarker : MonoBehaviour
 {
     public Transform MinimapCam;
-    public float MinimapRadius;
+    public float MinimapSize;
+    Vector3 TempPos;
 
-    void FixedUpdate()
-    { 
-        Vector3 centerPosition = MinimapCam.transform.localPosition;
+    void Update()
+    {
+        TempPos = transform.parent.transform.position;
+        TempPos.y = transform.position.y;
+        transform.position = TempPos;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 centerPosition = MinimapCam.transform.position;
         centerPosition.y -= 0.5f;
         float Distance = Vector3.Distance(transform.position, centerPosition);
 
-        if (Distance > MinimapRadius)
+        if (Distance > MinimapSize)
         {
             Vector3 fromOriginToObject = transform.position - centerPosition;
-            fromOriginToObject *= MinimapRadius / Distance;
+            fromOriginToObject *= MinimapSize / Distance;
             transform.position = centerPosition + fromOriginToObject;
         }
     }
