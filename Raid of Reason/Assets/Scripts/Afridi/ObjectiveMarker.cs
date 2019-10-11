7c47b088a43cb4b895078b6edd86d95a7ad24e06
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2019-10-11 11:51:27 +1100

    - Fixed UI Canvas

---
+++ Raid of Reason/Assets/Scripts/Afridi/ObjectiveMarker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ObjectiveMarker : MonoBehaviour
+{
+    public Transform MinimapCam;
+    public float MinimapRadius;
+
+    void FixedUpdate()
+    { 
+        Vector3 centerPosition = MinimapCam.transform.localPosition;
+        centerPosition.y -= 0.5f;
+        float Distance = Vector3.Distance(transform.position, centerPosition);
+
+        if (Distance > MinimapRadius)
+        {
+            Vector3 fromOriginToObject = transform.position - centerPosition;
+            fromOriginToObject *= MinimapRadius / Distance;
+            transform.position = centerPosition + fromOriginToObject;
+        }
+    }
+}
