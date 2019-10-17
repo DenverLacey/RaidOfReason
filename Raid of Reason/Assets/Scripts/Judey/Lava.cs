ed37b4960b2e3ba9ebcdec28e4bd6fe9ed2b7d3e
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2019-10-17 11:16:23 +1100

    Updated Lava.cs so it meets code standards

--- Raid of Reason/Assets/Scripts/Judey/Lava.cs
+++ Raid of Reason/Assets/Scripts/Judey/Lava.cs
@@ -1,67 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Author: Judy, Denver
+ * Description:	Controls lava position using timer and deals damage to players that are colliding
+ *				with the lava.
+ */
+
 using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
-    public GameObject nash;
-    public GameObject thea;
-    public GameObject ken;
+	[Tooltip("Damage dealt by lava every game tick")]
+	[SerializeField]
+    private float m_damage = 2;
 
-    public float damage = -34;
-    public float timer = 30f;
+	[Tooltip("Time it takes for lava to engulf level")]
+	[SerializeField]
+    private float m_timer = 30f;
+
+	private Kenron m_kenron;
+	private Kreiger m_nashorn;
+	private Thea m_thea;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		m_kenron = GameManager.Instance.Kenron;
+		m_nashorn = GameManager.Instance.Kreiger;
+		m_thea = GameManager.Instance.Thea;
+	}
 
     // Update is called once per frame
     void Update()
     {
-
-        timer -= Time.deltaTime;
+        m_timer -= Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Kenron" )
-        {
-            ken.GetComponent<Kenron>().m_currentHealth += damage;
-        }
-
-        if(other.tag == "Kreiger")
-        {
-            nash.GetComponent<Kreiger>().m_currentHealth += damage;
-        }
-
-        if (other.tag == "Thea")
-        {
-            thea.GetComponent<Thea>().m_currentHealth += damage;
-        }
-
+		DealDamage(other.tag);
     }
 
     private void OnTriggerStay(Collider other)
     {
-        {
-            if (other.tag == "Kenron")
-            {
-                ken.GetComponent<Kenron>().m_currentHealth += damage;
-            }
+		DealDamage(other.tag);
+		m_timer = 2f;
+	}
 
-            if (other.tag == "Kreiger")
-            {
-                nash.GetComponent<Kreiger>().m_currentHealth += damage;
-            }
+	void DealDamage(string tag)
+	{
+		switch (tag)
+		{
+			case "Kenron":
+				m_kenron.TakeDamage(m_damage);
+				break;
 
-            if (other.tag == "Thea")
-            {
-                thea.GetComponent<Thea>().m_currentHealth += damage;
-            }
+			case "Kreiger":
+				m_nashorn.TakeDamage(m_damage);
+				break;
 
-            timer = 2f;
-        }
-    }
+			case "Thea":
+				m_thea.TakeDamage(m_damage);
+				break;
+
+			default:
+				break;
+		}
+	}
 }
