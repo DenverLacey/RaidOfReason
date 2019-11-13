5681a87b17953403825cf1b821683917bc916a3d
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2019-11-13 13:43:44 +1100

    - Fixed Portal Transportation

--- Raid of Reason/Assets/Portal.cs
+++ Raid of Reason/Assets/Portal.cs
@@ -8,58 +8,82 @@ public class Portal : MonoBehaviour
     public int buildIndex;
     public float transitonTime;
 
+    private bool kenPass = false;
+    private bool nasPass = false;
+    private bool thePass = false;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (GameManager.Instance.Players.Count > 1) {
 
-            #region Single Player
-            if (Utility.IsPlayerAvailable(CharacterType.KENRON))
-            {
-                StartCoroutine(MovetoScene(transitonTime));
-            }
-            if (Utility.IsPlayerAvailable(CharacterType.KREIGER))
-            {
-                StartCoroutine(MovetoScene(transitonTime));
-            }
-            if (Utility.IsPlayerAvailable(CharacterType.THEA))
-            {
-                StartCoroutine(MovetoScene(transitonTime));
-            }
-            #endregion
-
             #region Two Player
             if (Utility.IsPlayerAvailable(CharacterType.KENRON) && Utility.IsPlayerAvailable(CharacterType.THEA))
             {
-                StartCoroutine(MovetoScene(transitonTime));
+                if (other.tag == "Kenron")
+                {
+                    kenPass = true;
+                }
+                if (other.tag == "Thea")
+                {
+                    thePass = true;
+                }
+                if (kenPass && thePass)
+                {
+                    StartCoroutine(MovetoScene(transitonTime));
+                }
             }
             if (Utility.IsPlayerAvailable(CharacterType.KENRON) && Utility.IsPlayerAvailable(CharacterType.KREIGER))
             {
-                StartCoroutine(MovetoScene(transitonTime));
+                if (other.tag == "Kenron")
+                {
+                    kenPass = true;
+                }
+                if (other.tag == "Kreiger")
+                {
+                    nasPass = true;
+                }
+                if (kenPass && nasPass)
+                {
+                    StartCoroutine(MovetoScene(transitonTime));
+                }
             }
-
             if (Utility.IsPlayerAvailable(CharacterType.KREIGER) && Utility.IsPlayerAvailable(CharacterType.THEA))
             {
-                StartCoroutine(MovetoScene(transitonTime));
-            }
-            if (Utility.IsPlayerAvailable(CharacterType.KREIGER) && Utility.IsPlayerAvailable(CharacterType.KENRON))
-            {
-                StartCoroutine(MovetoScene(transitonTime));
-            }
-
-            if (Utility.IsPlayerAvailable(CharacterType.THEA) && Utility.IsPlayerAvailable(CharacterType.KREIGER))
-            {
-                StartCoroutine(MovetoScene(transitonTime));
-            }
-            if (Utility.IsPlayerAvailable(CharacterType.THEA) && Utility.IsPlayerAvailable(CharacterType.KENRON))
-            {
-                StartCoroutine(MovetoScene(transitonTime));
+                if (other.tag == "Thea")
+                {
+                    thePass = true;
+                }
+                if (other.tag == "Kreiger")
+                {
+                    nasPass = true;
+                }
+                if (thePass && nasPass)
+                {
+                    StartCoroutine(MovetoScene(transitonTime));
+                }
             }
             #endregion
 
             #region Three Player
             if (Utility.IsPlayerAvailable(CharacterType.KENRON) && Utility.IsPlayerAvailable(CharacterType.THEA) && Utility.IsPlayerAvailable(CharacterType.KREIGER))
             {
-                StartCoroutine(MovetoScene(transitonTime));
+                if (other.tag == "Thea")
+                {
+                    thePass = true;
+                }
+                if (other.tag == "Kreiger")
+                {
+                    nasPass = true;
+                }
+                if (other.tag == "Kenron")
+                {
+                    kenPass = true;
+                }
+                if (thePass && nasPass && kenPass)
+                {
+                    StartCoroutine(MovetoScene(transitonTime));
+                }
             }
             #endregion
         }
@@ -67,6 +91,9 @@ public class Portal : MonoBehaviour
 
     IEnumerator MovetoScene(float time)
     {
+        kenPass = false;
+        nasPass = false;
+        thePass = false;
         yield return new WaitForSeconds(time);
 		LevelManager.FadeLoadLevel(buildIndex);
     }
