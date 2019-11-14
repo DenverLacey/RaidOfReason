e085caa018b649ed3bb9e563536b6c5b68fbb41a
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2019-11-14 14:27:50 +1100

    respawn effect implemented

---
+++ Raid of Reason/Assets/Scripts/Denver/RespawnEffectActor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnEffectActor : MonoBehaviour
+{
+	public delegate void OnEvent(RespawnEffectActor respawnEffectActor);
+
+	[Tooltip("Build up particle effect")]
+	[SerializeField]
+	private GameObject m_buildUpPrefab;
+
+	[Tooltip("Respawn particle effect")]
+	[SerializeField]
+	private GameObject m_respawnPrefab;
+
+	private float m_respawnEffectDelay;
+
+	private float m_timer;
+
+	public OnEvent onSpawn;
+	public OnEvent onDeactivate;
+
+	[HideInInspector]
+	public BaseCharacter character;
+
+	private void Start()
+	{
+		Deactivate();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		m_timer += Time.deltaTime;
+
+		if (m_timer >= m_respawnEffectDelay)
+		{
+			m_respawnPrefab.SetActive(true);
+			onSpawn(this);
+		}
+
+		if (m_timer >= m_respawnEffectDelay + 4f)
+		{
+			Deactivate();
+			onDeactivate(this);
+		}
+    }
+
+	public void Activate(float respawnDelay)
+	{
+		gameObject.SetActive(true);
+		m_buildUpPrefab.SetActive(true);
+		m_respawnEffectDelay = respawnDelay; ;
+	}
+
+	public void Deactivate()
+	{
+		m_timer = 0f;
+		character = null;
+		m_buildUpPrefab.SetActive(false);
+		m_respawnPrefab.SetActive(false);
+		gameObject.SetActive(false);
+	}
+}
--- Raid of Reason/Assets/Scripts/Denver/RespawnManager.cs
+++ Raid of Reason/Assets/Scripts/Denver/RespawnManager.cs
@@ -32,8 +32,15 @@ public class RespawnManager : MonoBehaviour
     [SerializeField]
     private DeathMenu m_deathScreen;
 
+	[Tooltip("Respawn Effect Prefab")]
+	[SerializeField]
+	private GameObject m_respawnEffectObject;
+
 	private static RespawnManager ms_instance;
-	private static Dictionary<BaseCharacter, RespawnInformation> m_respawnInformation = new Dictionary<BaseCharacter, RespawnInformation>();
+	private static Dictionary<BaseCharacter, RespawnInformation> m_respawnInformation;
+
+	private static Queue<RespawnEffectActor> m_inactiveRespawnEffects;
+	private static List<RespawnEffectActor> m_activeRespawnEffects;
 
 	private void Awake()
 	{
@@ -42,9 +49,19 @@ public class RespawnManager : MonoBehaviour
 
 	private void Start()
 	{
+		m_respawnInformation = new Dictionary<BaseCharacter, RespawnInformation>();
+		m_inactiveRespawnEffects = new Queue<RespawnEffectActor>();
+		m_activeRespawnEffects = new List<RespawnEffectActor>();
+
 		foreach (var player in GameManager.Instance.Players)
 		{
 			m_respawnInformation.Add(player, new RespawnInformation(player.transform.position));
+			RespawnEffectActor respawnEffectActor = Instantiate(m_respawnEffectObject).GetComponent<RespawnEffectActor>();
+
+			respawnEffectActor.onSpawn = OnSpawnEvent;
+			respawnEffectActor.onDeactivate = OnDeactivate;
+
+			m_inactiveRespawnEffects.Enqueue(respawnEffectActor);
 		}
 	}
 
@@ -87,8 +104,14 @@ public class RespawnManager : MonoBehaviour
 	private static IEnumerator WaitToRespawn(BaseCharacter player)
 	{
 		yield return new WaitForSeconds(ms_instance.m_respawnDelay);
-		player.SoftActivate();
-		player.transform.position = m_respawnInformation[player].respawnPosition;
+
+		RespawnEffectActor respawnEffect = m_inactiveRespawnEffects.Dequeue();
+		respawnEffect.Activate(2f);
+		respawnEffect.character = player;
+		respawnEffect.transform.position = m_respawnInformation[player].respawnPosition;
+		m_respawnInformation[player].isRespawning = true;
+
+		m_activeRespawnEffects.Add(respawnEffect);
 	}
 
 	private static bool AllRespawning()
@@ -108,5 +131,19 @@ public class RespawnManager : MonoBehaviour
 		Vector3 position = source;
 		source.y = character.transform.position.y;
 		return position;
+	}
+
+	private void OnSpawnEvent(RespawnEffectActor respawnEffectActor)
+	{
+		BaseCharacter character = respawnEffectActor.character;
+
+		character.SoftActivate();
+		character.transform.position = m_respawnInformation[character].respawnPosition;
+	}
+
+	private void OnDeactivate(RespawnEffectActor respawnEffectActor)
+	{
+		m_activeRespawnEffects.Remove(respawnEffectActor);
+		m_inactiveRespawnEffects.Enqueue(respawnEffectActor);
 	}
 }
