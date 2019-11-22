ae6f475802e7a48e0d1b9d32189a2291260406bc
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2019-11-22 16:58:55 +1100

    NothingBehaviourTree now will attack when Taunted

--- Raid of Reason/Assets/Scripts/Denver/Behaviour Tree/NothingBehaviourTree.cs
+++ Raid of Reason/Assets/Scripts/Denver/Behaviour Tree/NothingBehaviourTree.cs
@@ -5,7 +5,28 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Nothing Behaviour Tree", menuName = "Behaviour Trees/Behaviour Tree - Nothing")]
 public class NothingBehaviourTree : BehaviourTree
 {
+	Selector m_behaviourTree = new Selector();
+
+	NothingBehaviourTree()
+	{
+		Sequence attackSequence = new Sequence();
+		attackSequence.AddChild(new ViewRangeCondition());
+		attackSequence.AddChild(new SightlineCondition());
+		attackSequence.AddChild(new MaxAttackRangeCondition());
+		attackSequence.AddChild(new MeleeEnemyAttack());
+
+		Sequence tauntSequence = new Sequence();
+		tauntSequence.AddChild(new TauntEvent());
+		tauntSequence.AddChild(attackSequence);
+
+		StopPathing stopPathing = new StopPathing();
+
+		m_behaviourTree.AddChild(tauntSequence);
+		m_behaviourTree.AddChild(stopPathing);
+	}
+
 	public override void Execute(EnemyData agent)
 	{
+		m_behaviourTree.Execute(agent);
 	}
 }
