2bb3b32505f453c75b9dee94905bd1f9ef0099d8
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2019-11-22 15:52:27 +1100

    - GIFLoader.cs completed for the game

---
+++ Raid of Reason/Assets/Scripts/Afridi/GIFLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GIFLoader : MonoBehaviour
+{
+    public Sprite[] textures;
+    public Image image;
+    public int speedOfPlay;
+
+    private void Update()
+    {
+        if (image != null && textures.Length > 0)
+        {
+            int index = Mathf.RoundToInt(Time.time * speedOfPlay);
+            index = index % textures.Length;
+            if (index != textures.Length)
+            {
+                image.sprite = textures[index];
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+    }
+}
+
