d26f786909c489f3e5705a00bea0dcd626c4f40c
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2019-11-28 16:07:43 +1100

    - Added Tips To The Level 0.unity

---
+++ Raid of Reason/Assets/Scripts/Afridi/TipManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class TipManager : MonoBehaviour
+{
+    public List<GameObject> Tips = new List<GameObject>();
+    private bool isTriggered = false;
+
+    private void Awake()
+    {
+        if (Tips.Count > 0)
+        {
+            foreach (GameObject tip in Tips)
+            {
+                tip.gameObject.SetActive(false);
+            }
+            Tips[0].SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (Tips.Count > 0)
+        {
+            Time.timeScale = 0.0f;
+            if (XCI.GetButtonDown(XboxButton.A, XboxController.Any) && !isTriggered)
+            {
+                if (Tips[0] != null && !isTriggered)
+                {
+                    Tips[0].SetActive(false);
+                    Tips.RemoveAt(0);
+
+                    if (Tips.Count > 0)
+                    {
+                        Tips[0].SetActive(true);
+                    }
+                    if (Tips.Count == 0)
+                    {
+                        Time.timeScale = 1f;
+                    }
+
+                    isTriggered = true;
+                }
+            }
+            else
+            {
+                isTriggered = false;
+            }      
+        }
+    }
+}
